833aaddaa86f5488d6e5e9b93d84afe0a30e38af
Marcus Jensen <marcusjensen1515@gmail.com>
2020-01-10 09:47:26 -0800

    get title for given project passing

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,15 @@
 class Project
+  attr_accessor :title, :id
 
   def initialize(attributes)
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id).to_i
 
+  end
+
+  def title
+    returned_title = @title
+    returned_title
   end
 
 
